466701e24087d41a65a3d6942b77d45639cb7c19
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-05-24 09:13:21 -0400

    Add initial project structure and configuration files
    Created a new Visual Studio solution and project files for WSBSD NetBSd 10.1. Added App.config for application settings, Program.cs with a class definition, and AssemblyInfo.cs for assembly metadata. Configured Debug and Release settings in the project file.

---
+++ WSBSD/BSDDISTROS/NetBSD/10.1/root/WSBSD NetBSd 10.1/WSBSD NetBSd 10.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices; // Required for DllImport
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+
+class Program
+{
+
+}
